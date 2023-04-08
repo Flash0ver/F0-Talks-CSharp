39b829e08ccdff132e13eed30162e415833f393e
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2023-04-08 04:25:24 +0200

    talk: Warsaw IT Days 2023

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # F0-Talks-CSharp
 My talks on the sweet syntax of C#.
+
+## The sweet Syntax of C# 11
+
+### 2023-03-31: Warsaw IT Days 2023 (VoD)
+- [Warsaw IT Days](https://warszawskiedniinformatyki.pl/en/)
---
+++ code/F0.Talks.CSharp.CSharp11/Lang/OtherFile.cs
@@ -0,0 +1,10 @@
+namespace F0.Talks.CSharp.CSharp11.Lang;
+
+internal class OtherFile //: IFileScopedInterface
+{
+    public void MyOtherMethod()
+    {
+        _ = new FileTypes().MyMethod();
+        //_ = new FileLocalType().Text;
+    }
+}
